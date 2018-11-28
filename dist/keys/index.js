11a1390ec5d4588b845cd9d90223160b4225ace9
Weiliang Li <to.be.impressive@gmail.com>
2018-11-28 19:44:32 +0900

    [Fix] ignore dist dir

--- dist/keys/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var PrivateKey_1 = require("./PrivateKey");
-exports.PrivateKey = PrivateKey_1["default"];
-var PublicKey_1 = require("./PublicKey");
-exports.PublicKey = PublicKey_1["default"];
-//# sourceMappingURL=index.js.map
