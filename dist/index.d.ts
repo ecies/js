fdb600c5c2c784de9e0a6f99a942e808719bb142
Weiliang Li <to.be.impressive@gmail.com>
2018-11-28 19:47:02 +0900

    [Fix] ignore dist dir (#6)

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-export declare function encrypt(receiverPubhex: string, msg: Buffer): Buffer;
-export declare function decrypt(receiverPrvhex: string, msg: Buffer): Buffer;
