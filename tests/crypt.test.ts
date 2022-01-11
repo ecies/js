702fcf263d5ce5b6f9562ecc89c267cf521ec1c9
Weiliang Li <to.be.impressive@gmail.com>
2022-01-11 20:42:08 +0900

    bump dependencies

--- tests/crypt.test.ts
+++ tests/crypt.test.ts
@@ -4,7 +4,7 @@ import { randomBytes } from "crypto";
 import { stringify } from "querystring";
 
 import { decrypt, encrypt } from "../src/index";
-import { PrivateKey } from "../src/keys";
+import { PrivateKey, PublicKey } from "../src/keys";
 import { aesDecrypt, aesEncrypt, decodeHex } from "../src/utils";
 
 const PYTHON_BACKEND = "https://eciespy.herokuapp.com/";
@@ -53,6 +53,17 @@ describe("test encrypt and decrypt", () => {
     const prv2 = new PrivateKey();
     const encrypted2 = encrypt(prv2.publicKey.toHex(), Buffer.from(TEXT));
     expect(decrypt(prv2.toHex(), encrypted2).toString()).to.be.equal(TEXT);
+  });
+
+  it("tests sk pk", () => {
+    const sk = PrivateKey.fromHex(
+      "5b5b1a0ff51e4350badd6f58d9e6fa6f57fbdbde6079d12901770dda3b803081"
+    );
+    const pk = PublicKey.fromHex(
+      "048e41409f2e109f2d704f0afd15d1ab53935fd443729913a7e8536b4cef8cf5773d4db7bbd99e9ed64595e24a251c9836f35d4c9842132443c17f6d501b3410d2"
+    );
+    const enc = encrypt(pk.toHex(), Buffer.from(TEXT));
+    expect(decrypt(sk.toHex(), enc).toString()).to.be.equal(TEXT);
   });
 
   it("tests encrypt/decrypt against python version", () => {
