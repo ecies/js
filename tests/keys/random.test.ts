b82c085e2cb71165d0db0985e4a6310f79e7ff19
Weiliang Li <to.be.impressive@gmail.com>
2024-09-16 01:51:18 +0900

    Bump dependencies (#791)

--- tests/keys/random.test.ts
+++ tests/keys/random.test.ts
@@ -10,19 +10,23 @@ describe("test random keys", () => {
     const skFromHex = PrivateKey.fromHex(sk.toHex());
     const pkFromHex = PublicKey.fromHex(sk.publicKey.toHex(false));
 
-    expect(pkFromHex).toStrictEqual(sk.publicKey);
     expect(skFromHex).toStrictEqual(sk);
+    expect(pkFromHex).toStrictEqual(sk.publicKey);
   }
 
   it("tests secp256k1", () => {
     testRandom();
   });
 
-  it("tests curve25519", () => {
+  it("tests x25519", () => {
     ECIES_CONFIG.ellipticCurve = "x25519";
 
     testRandom();
 
+    ECIES_CONFIG.ellipticCurve = "secp256k1";
+  });
+
+  it("tests ed25519", () => {
     ECIES_CONFIG.ellipticCurve = "ed25519";
 
     testRandom();
