0671d429e120754a8d695a2064b388b185f2fd0c
Weiliang Li <to.be.impressive@gmail.com>
2024-10-18 01:10:12 +0900

    Revamp examples (#800)

--- example/browser/script.js
+++ example/browser/script.js
@@ -4,42 +4,41 @@ import { PrivateKey, decrypt, encrypt } from "eciesjs";
 
 import "./style.css";
 
-globalThis.Buffer = Buffer
+globalThis.Buffer = Buffer;
 
 const sk = new PrivateKey();
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
 export function setup(encryptedElement, textElement, decryptedElement) {
-  const text = "hello eciesjs🔒"
+  const text = "hello eciesjs🔒";
   let encrypted;
 
-  encryptedElement.innerHTML = `click me to encrypt`
-  textElement.innerHTML = text
-  decryptedElement.innerHTML = `click me to decrypt`
+  encryptedElement.innerHTML = `click me to encrypt`;
+  textElement.innerHTML = text;
+  decryptedElement.innerHTML = `click me to decrypt`;
 
   const _encrypt = () => {
-    encrypted = encrypt(sk.publicKey.toHex(), encoder.encode(text))
-    encryptedElement.innerHTML = `encrypted:`
-    textElement.innerHTML = `${bytesToHex(encrypted)}`
-    decryptedElement.innerHTML = `click me to decrypt`
-  }
+    encrypted = encrypt(sk.publicKey.toHex(), encoder.encode(text));
+    encryptedElement.innerHTML = `encrypted:`;
+    textElement.innerHTML = `${bytesToHex(encrypted)}`;
+    decryptedElement.innerHTML = `click me to decrypt`;
+  };
   const _decrypt = () => {
-    encryptedElement.innerHTML = `click me to encrypt`
+    encryptedElement.innerHTML = `click me to encrypt`;
     if (encrypted) {
-      textElement.innerHTML = `${decoder.decode(decrypt(sk.secret, encrypted))}`
-      decryptedElement.innerHTML = `decrypted:`
-      encrypted = undefined
+      textElement.innerHTML = `${decoder.decode(decrypt(sk.secret, encrypted))}`;
+      decryptedElement.innerHTML = `decrypted:`;
+      encrypted = undefined;
     } else {
-      textElement.innerHTML = "click encrypt button first"
+      textElement.innerHTML = "click encrypt button first";
     }
-  }
-  encryptedElement.addEventListener('click', () => _encrypt())
-  decryptedElement.addEventListener('click', () => _decrypt())
+  };
+  encryptedElement.addEventListener("click", () => _encrypt());
+  decryptedElement.addEventListener("click", () => _decrypt());
 }
 
-
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <h1>Hello eciesjs!</h1>
     <div class="card">
@@ -48,7 +47,10 @@ document.querySelector('#app').innerHTML = `
     </div>
     <p id="text"></p>
   </div>
-`
+`;
 
-setup(document.querySelector('#encrypted'), document.querySelector('#text'),
-  document.querySelector('#decrypted'))
+setup(
+  document.querySelector("#encrypted"),
+  document.querySelector("#text"),
+  document.querySelector("#decrypted")
+);
