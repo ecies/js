17a4b39b07a3c75e435c6cf88599ad898b37e38b
Weiliang Li <to.be.impressive@gmail.com>
2024-10-28 18:09:03 +0900

    Revamp elliptic utils

--- example/browser/vite.config.ts
+++ example/browser/vite.config.ts
@@ -5,8 +5,12 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("@noble")) {
-            return "noble";
+          if (id.includes("@noble/curves")) {
+            return "noble-curves";
+          } else if (id.includes("@noble/ciphers")) {
+            return "noble-ciphers";
+          } else if (id.includes("@noble/hashes")) {
+            return "noble-hashes";
           } else if (id.includes("buffer")) {
             return "buffer";
           }
