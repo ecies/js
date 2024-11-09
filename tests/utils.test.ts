48cb8dc7c030ef4a68d6c4672e2e8514d593eac5
Weiliang Li <to.be.impressive@gmail.com>
2024-11-09 12:51:30 +0900

    [v0.3] Revamp tests (#813)

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -6,19 +6,19 @@ import { decodeHex, getValidSecret, remove0x } from "../src/utils";
 
 describe("test string <-> buffer utils ", () => {
   it("should remove 0x", () => {
-    expect(remove0x("0x0011")).toEqual("0011");
-    expect(remove0x("0011")).toEqual("0011");
-    expect(remove0x("0X0022")).toEqual("0022");
-    expect(remove0x("0022")).toEqual("0022");
+    expect(remove0x("0x0011")).toStrictEqual("0011");
+    expect(remove0x("0011")).toStrictEqual("0011");
+    expect(remove0x("0X0022")).toStrictEqual("0022");
+    expect(remove0x("0022")).toStrictEqual("0022");
   });
 
   it("should generate valid secret", () => {
     const key = getValidSecret();
-    expect(secp256k1.privateKeyVerify(key)).toEqual(true);
+    expect(secp256k1.privateKeyVerify(key)).toBe(true);
   });
 
   it("should convert hex to buffer", () => {
     const decoded = decodeHex("0x0011");
-    expect(decoded.equals(Buffer.from([0, 0x11]))).toEqual(true);
+    expect(decoded).toStrictEqual(Buffer.from([0, 0x11]));
   });
 });
